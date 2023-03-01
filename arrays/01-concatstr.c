95a56dca0cde4a8ba014019176385d189a992ae6
Simon Muia <simonmuia38@gmail.com>
2023-03-01 12:21:08 +0300

    print series of characters in an string array

---
+++ arrays/01-concatstr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Program to concat series of inputs on string
+ * main - entry point
+ * Return: 0 always
+*/
+
+int main ()
+{
+    char name[5] = {'S', 'i', 'm', 'o', 'n'};
+    // print the string
+    printf("String combined : %s\n", name);
+    return 0;
+}
